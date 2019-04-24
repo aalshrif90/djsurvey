b69b5b54cdc1b851ef1f6d83dfbffbda3cf64771
Abdullah Alsharif <gxr199@gmail.com>
2019-04-24 16:38:02 +0100

    Model --- refactoring few thing. I need to think of a general structure.

--- controlled/models.py
+++ controlled/models.py
@@ -3,16 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# types
-question_types = (
-    ('BACKGROUND', 'Background'),
-    ('ALL', 'All'),
-    ('ASSERT', 'Assertion'),
-    ('practice', 'Practice'),
-    ('EXIT', 'Exit')
-)
-
-# types
+# assertions
 assertions = (
     ('T', 'TURE'),
     ('F', 'FALSE'),
@@ -71,7 +62,6 @@ class Participants(models.Model):
 
     def __str__(self):
         return "Group %s - %s" % (self.group, self.pcode)
-        #return self.group + "-" + self.user_code
 
 # Question Structure
 class QuestionCategory(models.Model):
@@ -92,40 +82,22 @@ class Question(models.Model):
     question_category = models.ForeignKey(QuestionCategory, help_text = u"Choose a Question Category", null=True, blank=True)
     question_text = models.ForeignKey(QuestionText, help_text = u"Choose a Question", null=True, blank=True)
     code = models.TextField(help_text=u"Code Associated with the Question", null=True, blank=True)
-    is_it_singlechoice = models.BooleanField(default=False, help_text=u"One choice e.g. True or False?")
-    is_it_multichoices = models.BooleanField(default=False, help_text=u"multiple choices?")
-    is_it_dropdown = models.BooleanField(default=False, help_text=u"Drop Down?")
-    is_it_opinion = models.BooleanField(default=False, help_text=u"Requiring Opinion?")
+    singlechoice = models.BooleanField(default=False, help_text=u"One choice e.g. True or False?")
+    multichoices = models.BooleanField(default=False, help_text=u"multiple choices?")
+    opinion = models.BooleanField(default=False, help_text=u"Requiring Opinion?")
 
     def __str__(self):
         return self.pk
 
-class Dropdown(models.Model):
-    question = models.ForeignKey(Question, related_name="dropdown", help_text = u"Choose a question for this answer")
-    dropdown_text = models.TextField(help_text=u"Added your Dropdown text here.")
-
-    def __str__(self):
-        return self.dropdown_text
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, help_text = u"Choose a question for this answer")
-    choice_text = models.TextField(help_text=u"Added your Choice text here.")
-    right_answer = models.BooleanField(default=False, help_text=u"Is this a right answer?")
-
-    def __str__(self):
-        return self.choice_text
-
 # Storing Answers
 class Answer(models.Model):
-    question = models.ForeignKey(Question, help_text = u"the question answered")
-    user_code = models.CharField(max_length=255, help_text=u"The code given to a user")
-    time_duration = models.BigIntegerField(help_text=u"The duration time (Millisconds) to answer the question")
-    answer = models.CharField(max_length=255, help_text=u"The answer. Either from a dropdown or choice")
-    is_it_right = models.BooleanField(default=False, help_text=u"Did the test taker get it right?")
-    question_type = models.CharField(max_length=255, help_text=u"The Question type")
-    tech_type = models.CharField(max_length=255, help_text=u"Technique Type if any")
+    question = models.ForeignKey(Question, help_text = u"Question Answered")
+    pcode = models.CharField(max_length=255, help_text=u"Participant Code")
+    duration = models.BigIntegerField(help_text=u"Answering Duration (Millisconds)")
+    answer = models.CharField(max_length=255, help_text=u"The answer")
+    score = models.BooleanField(default=False, help_text=u"Right or Wrong")
     timestamp = models.DateTimeField(auto_now_add=True, help_text=u"Timestamp of the answer")
-    opinion_text = models.TextField(null=True,blank=True,help_text=u"opinion text")
+    opinion = models.TextField(null=True,blank=True,help_text=u"Participant Opinion")
 
     def __str__(self):
         return str(self.pk)
